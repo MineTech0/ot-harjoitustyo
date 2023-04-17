8ea00d2eb86c81c3fb47ecfb8956a6d8b16c88d8
Niilo Kurki <niilo.kurki@helsinki.fi>
2023-04-17 16:46:14 +0300

    added pylit and formating

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from invoke import task
 
+
 @task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
@@ -7,16 +8,29 @@ def start(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
-    
+
+
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
-    
+
+
 @task
 def init_db(ctx):
     ctx.run("python3 src/init_db.py", pty=True)
+
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
