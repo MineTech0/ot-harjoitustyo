70b200c844363b55735e0731d022709fd35a462c
Niilo Kurki <niilo.kurki@helsinki.fi>
2023-04-18 13:47:34 +0300

    fixed db reset and connection

--- src/init_db.py
+++ src/init_db.py
@@ -1,21 +1,59 @@
-import db
+import importlib
+import inspect
+import os
 from sqlite3 import Connection
+import db
+from entities.base_entity import BaseEntity
+from config import DATABASE_FILE_PATH
+
 
 def create_tables(connection: Connection):
-    print("Creating tables...")
-    pass
-
-def drop_tables(connection: Connection):
-    print("Dropping tables...")
-    pass
+    """
+    Creates alla tables from the entities folder.
     
+    *generated with ChatGPT*
+
+    Args:
+        connection (Connection): db connection
+    """
+    print("Creating tables...")
+    # Get a list of all .py files in the entities folder
+    entity_files = [f for f in os.listdir("src/entities") if f.endswith(".py")]
+
+    # Loop through the files and import the modules dynamically
+    for file in entity_files:
+        module_name = file[:-3]  # Remove the .py extension
+        module = importlib.import_module(f"entities.{module_name}")
+
+        # Loop through the classes in the module and create the tables
+        for _, obj in inspect.getmembers(module, inspect.isclass):
+            if issubclass(obj, BaseEntity):
+                sql_statement = obj.create_table()
+                if sql_statement:
+                    print(f"Executing: {sql_statement}")
+                    connection.execute(sql_statement)
+                    connection.commit()
+
+
+def drop_db():
+    """
+    Deletes the database
+    
+    """
+    print("Deleting database...", DATABASE_FILE_PATH)
+    path = os.path.join(DATABASE_FILE_PATH)
+    if os.path.exists(path):
+        os.remove(path)
+
+
 def init_database():
 
+    drop_db()
+    
     connection = db.get_connection()
 
-    drop_tables(connection)
     create_tables(connection)
-    
+
     print("Database initialized successfully.")
 
 
