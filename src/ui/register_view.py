d3170a304bbf6304843397d504a3d96097a9ce8f
Niilo Kurki <niilo.kurki@helsinki.fi>
2023-04-24 18:07:07 +0300

    enhanced funktionality and style

--- src/ui/register_view.py
+++ src/ui/register_view.py
@@ -8,8 +8,13 @@ class RegisterView(BaseView):
 
     def _initialize(self):
         username_label = ttk.Label(
-            master=self._frame, text="Syötä käyttäjänimi:")
-        password_label = ttk.Label(master=self._frame, text="Syötä salasana:")
+            master=self._frame, 
+            text="Syötä käyttäjänimi:"
+        )
+        password_label = ttk.Label(
+            master=self._frame, 
+            text="Syötä salasana:"
+        )
 
         self._username_input = ttk.Entry(master=self._frame)
         self._password_input = ttk.Entry(master=self._frame, show="*")
@@ -20,13 +25,23 @@ class RegisterView(BaseView):
             command=self._handle_register
         )
 
-        username_label.grid(row=0, column=0)
-        password_label.grid(row=1, column=0)
-        self._username_input.grid(row=0, column=1)
-        self._password_input.grid(row=1, column=1)
-        button.grid(row=2, column=1)
+        # Add padding and spacing between the elements
+        username_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        password_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        self._username_input.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+        self._password_input.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
+        button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
 
-    def _handle_register(self):
+        # Bind the "Enter" key to the input fields to submit the form
+        self._password_input.bind("<Return>", self._handle_register)
+        
+        self._username_input.focus_set()
+
+
+    def _handle_register(self,event=None):
+        
+        if not self.validate_inputs():
+            return
 
         user_name = self._username_input.get()
         password = self._password_input.get()
@@ -36,3 +51,12 @@ class RegisterView(BaseView):
         self.show_info("Rekisteröityminen onnistui")
 
         self.navigate('login')
+        
+    def validate_inputs(self):
+        if len(self._username_input.get()) < 3:
+            self.show_error("Käyttäjänimen tulee olla vähintään 3 merkkiä pitkä")
+            return False
+        if len(self._password_input.get()) < 5:
+            self.show_error("Salasanan tulee olla vähintään 5 merkkiä pitkä")
+            return False
+        return True
