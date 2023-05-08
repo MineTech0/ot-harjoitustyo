6557b32d68e9735a36758d1f958b098002dec2e2
Niilo Kurki <niilo.kurki@helsinki.fi>
2023-05-08 14:17:41 +0300

    added tests

--- src/tests/account_service_test.py
+++ src/tests/account_service_test.py
@@ -1,7 +1,7 @@
 import unittest
 from init_db import init_database
 
-from services.account_service import account_service
+from services.account_service import AccountNameTakenException, account_service
 from services.user_service import user_service
 
 
@@ -28,3 +28,16 @@ class TestAccountService(unittest.TestCase):
         self.assertEqual(account.name, "test")
         self.assertEqual(account.user_name, "test")
         self.assertEqual(account.password, "test")
+        
+    def test_delete_account(self):
+        self._account_service.add_account("test", "test", "test")
+        self._account_service.delete_account("test")
+        names = self._account_service.get_all_account_names()
+        self.assertEqual(len(names), 0)
+
+        self.assertEqual(len(names), 0)
+        
+    def test_no_same_name_account(self):
+        self._account_service.add_account("test", "test", "test")
+        self.assertRaises(
+            AccountNameTakenException, self._account_service.add_account, "test", "test", "test")
--- src/tests/user_service_test.py
+++ src/tests/user_service_test.py
@@ -1,7 +1,7 @@
 import unittest
 from init_db import init_database
 
-from services.user_service import user_service
+from services.user_service import UserNameTakenException, user_service
 
 
 class TestUserService(unittest.TestCase):
@@ -21,3 +21,22 @@ class TestUserService(unittest.TestCase):
         self.assertTrue(self._user_service.login_user("test", "test"))
         self.assertFalse(self._user_service.login_user("test", "wrong"))
         self.assertFalse(self._user_service.login_user("wrong", "test"))
+
+    def test_no_same_username(self):
+        self._user_service.create_user("test", "test")
+        self.assertRaises(UserNameTakenException,
+                          self._user_service.create_user, "test", "test")
+
+    def test_logout(self):
+        self._user_service.create_user("test", "test")
+        self._user_service.login_user("test", "test")
+        self._user_service.logout_user()
+        self.assertIsNone(self._user_service.encryption_password)
+
+    def test_get_encryption_password(self):
+        self._user_service.create_user("test", "test")
+        self._user_service.login_user("test", "test")
+        self.assertEqual(self._user_service.get_encryption_password(), "test")
+        self._user_service.logout_user()
+        self.assertRaises(
+            ValueError, self._user_service.get_encryption_password)
